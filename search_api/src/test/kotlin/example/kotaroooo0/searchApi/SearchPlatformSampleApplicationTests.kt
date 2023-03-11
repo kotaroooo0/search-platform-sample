e00d490cad27c8078506bad945d41764905d9a9c
Kotaro Adachi <k33asby@gmail.com>
2023-03-11 17:01:38 +0900

    initialize spring boot application

---
+++ search_api/src/test/kotlin/example/kotaroooo0/searchApi/SearchPlatformSampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package example.kotaroooo0.searchApi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SearchPlatformSampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
