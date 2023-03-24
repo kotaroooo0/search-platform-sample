41ee7e2e5a06d1ddb4ed6ad9fd00207f296678ed
Kotaro Adachi <k33asby@gmail.com>
2023-03-24 22:39:06 +0900

    implement book repository impl for Solr

--- search_api/build.gradle.kts
+++ search_api/build.gradle.kts
@@ -20,6 +20,8 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	// https://mvnrepository.com/artifact/org.apache.solr/solr-solrj
+	implementation("org.apache.solr:solr-solrj:8.11.2")
 }
 
 tasks.withType<KotlinCompile> {
--- search_api/src/main/kotlin/example/kotaroooo0/searchApi/infrastructure/BookRepositoryImpl.kt
+++ search_api/src/main/kotlin/example/kotaroooo0/searchApi/infrastructure/BookRepositoryImpl.kt
@@ -3,17 +3,28 @@ package example.kotaroooo0.searchApi.infrastructure
 import example.kotaroooo0.searchApi.domain.objects.books.BookId
 import example.kotaroooo0.searchApi.domain.objects.books.SearchParameter
 import example.kotaroooo0.searchApi.domain.repository.BookRepository
+import org.apache.solr.client.solrj.SolrClient
+import org.apache.solr.client.solrj.beans.DocumentObjectBinder
+import org.apache.solr.client.solrj.response.QueryResponse
+import org.apache.solr.common.params.MapSolrParams
 import org.springframework.stereotype.Repository
 
 @Repository
-class BookRepositoryImpl : BookRepository {
+class BookRepositoryImpl(private val solrClient: SolrClient) : BookRepository {
+
+    companion object {
+        private const val BOOK_COLLECTION_NAME = "books"
+    }
+
     override fun search(searchParameter: SearchParameter): List<BookId> {
-        // TODO("Not yet implemented")
-        return listOf(
-            BookId("1"),
-            BookId("2"),
-            BookId("3"),
-            BookId("4"),
-        )
+        // TODO: searchParameterを使うクエリを書く
+        val queryParams = MapSolrParams(mapOf(
+            "q" to "*:*",
+            "rows" to "5"
+        ))
+
+        val response: QueryResponse = solrClient.query(BOOK_COLLECTION_NAME, queryParams)
+        val bookDocuments: List<BookDocument> = DocumentObjectBinder().getBeans(BookDocument::class.java, response.results)
+        return bookDocuments.map { it.bookId }
     }
 }
