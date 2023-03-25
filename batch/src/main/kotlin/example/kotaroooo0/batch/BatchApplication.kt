9b5dfe23ae3b2a73ba59472c7495d4e4d51a4c9b
Kotaro Adachi <k33asby@gmail.com>
2023-03-25 11:36:38 +0900

    initialize spring batch

---
+++ batch/src/main/kotlin/example/kotaroooo0/batch/BatchApplication.kt
@@ -0,0 +1,11 @@
+package example.kotaroooo0.batch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BatchApplication
+
+fun main(args: Array<String>) {
+	runApplication<BatchApplication>(*args)
+}
